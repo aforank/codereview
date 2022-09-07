b1444a6bde6e3c6e446d4381cc807244c8e6d60f
Ankit Sharma <anks@microsoft.com>
2022-09-07 11:51:43 +0530

    Ticket Booking Scenario Added

---
+++ TicketBooking/Helpers/ISeatLockProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketBooking.Helpers
+{
+    public interface ISeatLockProvider
+    {
+        public bool LockSeat(string seatNo);
+
+        public void UnLockSeat(string seatNo);
+    }
+}
---
+++ TicketBooking/Models/BookingRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketBooking.Models
+{
+    public class BookingRequest
+    {
+        public string CustomerId { get; set; }
+
+        public string SeatNo { get; set; }
+
+        public string SeatType { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime RequestDate { get; set; }
+    }
+}
