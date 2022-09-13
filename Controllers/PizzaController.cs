9741d84ea431881bbf3b56b997a38e5c332bdd0a
Rajesh Mag <ramarg@microsoft.com>
2022-09-13 15:17:38 +0530

    contoso-pizza - Initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+
+namespace ContosoPizza.Controllers
+{
+    /// <summary>
+    /// Pizza Controller.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PizzaController"/> class.
+        /// </summary>
+        public PizzaController()
+        {
+        }
+
+        /// <summary>
+        /// Gets all pizzas.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+            PizzaService.GetAll();
+
+        /// <summary>
+        /// Gets the specified pizza by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if(pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        /// <summary>
+        /// Creates the specified pizza.
+        /// </summary>
+        /// <param name="pizza">The pizza.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {            
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
+
+        /// <summary>
+        /// Updates the specified pizza by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="pizza">The pizza.</param>
+        /// <returns></returns>
+        [HttpPost("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+
+            var existingPizza = PizzaService.Get(id);
+            if(existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);           
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Deletes the specified pizza by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza is null)
+                return NotFound();
+
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+[![Deployment to UAT](https://github.com/david-iaggbs/ContosoPizza/actions/workflows/deploy-uat.yml/badge.svg)](https://github.com/david-iaggbs/ContosoPizza/actions/workflows/deploy-uat.yml)
+
+# ContosoPizza
+ https://docs.microsoft.com/en-us/learn/modules/build-web-api-aspnet-core/3-exercise-create-web-api
+
+ testing again from remote dev env
