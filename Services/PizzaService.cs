f776e8c9d8456d0df4b3692ee24f4a620a77f649
Rajesh Mag <ramarg@microsoft.com>
2022-09-13 15:17:38 +0530

    Code review changescontoso-pizza - Initial commit

---
+++ Services/PizzaService.cs
@@ -0,0 +1,84 @@
+using ContosoPizza.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ContosoPizza.Services
+{
+    /// <summary>
+    /// Pizza Service.
+    /// </summary>
+    public static class PizzaService
+    {
+        /// <summary>
+        /// Gets the pizzas.
+        /// </summary>
+        /// <value>
+        /// The pizzas.
+        /// </value>
+        static List<Pizza> Pizzas { get; }
+        /// <summary>
+        /// The next identifier
+        /// </summary>
+        static int nextId = 3;
+        /// <summary>
+        /// Initializes the <see cref="PizzaService"/> class.
+        /// </summary>
+        static PizzaService()
+        {
+            Pizzas = new List<Pizza>
+            {
+                new Pizza { Id = 1, Name = "Classic Italian", IsGlutenFree = false },
+                new Pizza { Id = 2, Name = "Veggie", IsGlutenFree = true }
+            };
+        }
+
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        public static List<Pizza> GetAll() => Pizzas;
+
+        /// <summary>
+        /// Gets the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public static Pizza Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+        /// <summary>
+        /// Adds the specified pizza.
+        /// </summary>
+        /// <param name="pizza">The pizza.</param>
+        public static void Add(Pizza pizza)
+        {
+            pizza.Id = nextId++;
+            Pizzas.Add(pizza);
+        }
+
+        /// <summary>
+        /// Deletes the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        public static void Delete(int id)
+        {
+            var pizza = Get(id);
+            if(pizza is null)
+                return;
+
+            Pizzas.Remove(pizza);
+        }
+
+        /// <summary>
+        /// Updates the specified pizza.
+        /// </summary>
+        /// <param name="pizza">The pizza.</param>
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+            if(index == -1)
+                return;
+
+            Pizzas[index] = pizza;
+        }
+    }
+}
